00596abe2c20962d679fe6a925c8b4079788101a
Emil2245 <emilmontiel@hotmail.com>
2023-11-13 13:33:51 -0500

    modificacion en la rama main

--- src/main/java/pa2_u1_p5_ev_deber/pa2_u1_p5_ev_deber/Pa2U1P5EvDeberApplication.java
+++ src/main/java/pa2_u1_p5_ev_deber/pa2_u1_p5_ev_deber/Pa2U1P5EvDeberApplication.java
@@ -1,13 +1,18 @@
 package pa2_u1_p5_ev_deber.pa2_u1_p5_ev_deber;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Pa2U1P5EvDeberApplication {
+public class Pa2U1P5EvDeberApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5EvDeberApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Primer cambio en el main");
+	}
 }
