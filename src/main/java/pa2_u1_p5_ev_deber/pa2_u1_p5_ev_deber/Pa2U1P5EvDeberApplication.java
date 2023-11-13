58857bc6fcb12a2388bf772d39bf8b5e48593c0c
Emil2245 <emilmontiel@hotmail.com>
2023-11-13 13:18:24 -0500

    first commit

---
+++ src/main/java/pa2_u1_p5_ev_deber/pa2_u1_p5_ev_deber/Pa2U1P5EvDeberApplication.java
@@ -0,0 +1,13 @@
+package pa2_u1_p5_ev_deber.pa2_u1_p5_ev_deber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P5EvDeberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5EvDeberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pa2_u1_p5_ev_deber/pa2_u1_p5_ev_deber/Pa2U1P5EvDeberApplicationTests.java
@@ -0,0 +1,13 @@
+package pa2_u1_p5_ev_deber.pa2_u1_p5_ev_deber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pa2U1P5EvDeberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
